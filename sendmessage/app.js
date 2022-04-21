c49f7238fde234ea0a9f6c9fdd4efd274e2f2ddd
tommy <tommy.dev@pgpg.la>
2022-04-21 15:11:47 +0800

    sendmessage: update format

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -1,24 +1,24 @@
 // Copyright 2018-2020Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk')
 
 const ddb = new AWS.DynamoDB.DocumentClient({
-  apiVersion: "2012-08-10",
+  apiVersion: '2012-08-10',
   region: process.env.AWS_REGION,
-});
+})
 
-const { TABLE_NAME } = process.env;
+const { TABLE_NAME } = process.env
 
 exports.handler = async (event) => {
-  let connectionData;
 
+  let connectionData
   try {
     connectionData = await ddb
       .scan({ TableName: TABLE_NAME, ProjectionExpression: "connectionId" })
       .promise();
   } catch (e) {
-    return { statusCode: 500, body: e.stack };
+    return { statusCode: 500, body: e.stack }
   }
 
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
@@ -35,24 +35,24 @@ exports.handler = async (event) => {
     try {
       await apigwManagementApi
         .postToConnection({ ConnectionId: connectionId, Data: message })
-        .promise();
+        .promise()
     } catch (e) {
       if (e.statusCode === 410) {
-        console.log(`Found stale connection, deleting ${connectionId}`);
+        console.log(`Found stale connection, deleting ${connectionId}`)
         await ddb
           .delete({ TableName: TABLE_NAME, Key: { connectionId } })
-          .promise();
+          .promise()
       } else {
-        throw e;
+        throw e
       }
     }
-  });
+  })
 
   try {
-    await Promise.all(postCalls);
+    await Promise.all(postCalls)
   } catch (e) {
-    return { statusCode: 500, body: e.stack };
+    return { statusCode: 500, body: e.stack }
   }
 
-  return { statusCode: 200, body: "Data sent." };
-};
+  return { statusCode: 200, body: 'Data sent.' }
+}
